a4af9c8bf25321797b03ba357eb57ff85a459152
ryu ishikawa <cvcl0808@gmail.com>
2015-01-26 12:05:12 +0900

    hoge

--- homes.rb
+++ homes.rb
@@ -1,36 +1,10 @@
-# require 'nokogiri'
-# require 'anemone'
-
-# opts = {
-# 	# storage: Anemone::Storage.MongoDB,
-# 	depth_limit: 0 
-# 		# 0 => 指定したURL先のみ
-# 		# 1 => 指定したURLにあるlinkから辿れる先も見る。
-# 		# false => 恐らく無限に飛ぶ...
-# 		# 「全部のurlから飛ばれちゃ終わらねーよ！」という場合は、focus_crawlで飛びたいURLを指定
-# }
-
-# Anemone.crawl("http://www.homes.co.jp/chintai/tokyo/city/price/", opts) do |anemone|
-# 	anemone.on_every_page do |page|
-# 		page.doc.xpath("/html//div[@class='priceList']//tbody[@id='prg-aggregate-graph']/tr").each do |node|
-# 			area  = node.xpath("./td[@class='area']/a/text()").to_s
-# 			price = node.xpath("./td[contains(@class,'price')]/div[contains(@class,'money')]/span/text()").to_s
-# 			puts area + "：" + price + "万円"
-# 			puts "-----------------\n"
-# 		end
-# 	end
-# end
-
 require 'nokogiri'
 require 'anemone'
 
 opts = {
-	# storage: Anemone::Storage.MongoDB,
 	depth_limit: 1
 		# 0 => 指定したURL先のみ
 		# 1 => 指定したURLにあるlinkから辿れる先も見る。
-		# false => 恐らく無限に飛ぶ...
-		# 「全部のurlから飛ばれちゃ終わらねーよ！」という場合は、focus_crawlで飛びたいURLを指定
 }
 
 Anemone.crawl("http://www.homes.co.jp/chintai/tokyo/city/price/", opts) do |anemone|		
